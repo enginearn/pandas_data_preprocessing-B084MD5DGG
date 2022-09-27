7bd45954fd5b6b5427146f2b94cbd0040e6f88bf
enginearn <enginyaaaaan@gmail.com>
2022-09-27 19:40:41 +0900

    Added

--- Chapter_02/data_structure_dataframe.py
+++ Chapter_02/data_structure_dataframe.py
@@ -22,3 +22,31 @@ print(df)
 dictionary = {'a': [1, 2, 3], 'b': [4, 5, 6], 'c': [7, 8, 9]}
 df = pd.DataFrame(dictionary)
 print(df)
+
+age = pd.Series([10, 12, 9], index=list('ABC'))
+sex = pd.Series(['M', 'F', 'F'], index=['C', 'A', 'D'])
+df = pd.DataFrame({'age': age, 'sex': sex})
+print(df)
+
+nest_dict = {
+    'age': {'A': 10, 'B': 12, 'C': 9},
+    'sex': {'C': 'M', 'A': 'F', 'D': 'F'}
+}
+
+df = pd.DataFrame(nest_dict)
+print(df)
+
+val = [[1, 2], [4, 5]]
+df1 = pd.DataFrame(val, columns=['A', 'B'], dtype=None)
+print(df1.dtypes)
+
+df2 = pd.DataFrame(val, columns=['A', 'B'], dtype=np.float64)
+print(df2.dtypes)
+
+df = pd.DataFrame({
+    'math': [82, 93, 77, 82, 0],
+    'english': [64, 48, 77, 61, 90],
+    'chemistry': [70, 65, 60, 90, 100],
+})
+
+print(df)
